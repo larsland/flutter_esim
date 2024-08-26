0b370da30d53e776d0c97a6e43efe06007317c45
Lars Liverød <lars.liverod@knowit.no>
2024-08-26 12:11:35 +0200

    Added more debug log properties

--- android/src/main/kotlin/com/hiennv/flutter_esim/FlutterEsimPlugin.kt
+++ android/src/main/kotlin/com/hiennv/flutter_esim/FlutterEsimPlugin.kt
@@ -96,7 +96,7 @@ class FlutterEsimPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
                 sendEvent("success", HashMap())
             } else if (resultCode == EuiccManager.EMBEDDED_SUBSCRIPTION_RESULT_ERROR) {
                 val detailedErrorCode = EuiccManager.EXTRA_EMBEDDED_SUBSCRIPTION_DETAILED_CODE
-                val detailsBody = hashMapOf("errorCode" to detailedErrorCode)
+                val detailsBody = hashMapOf("resultCode" to getResultCode(), "resultData" to getResultData(), "resultExtras" to getResultExtras())
                 sendEvent("fail", detailsBody)
             } else {
                 // Unknown Error
